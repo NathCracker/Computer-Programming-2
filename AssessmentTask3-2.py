3e2a46ce085e2f8ea5fbfa0ec37f4eb58fff5a68
Joe mama <101397760+NathCracker@users.noreply.github.com>
2022-10-31 14:51:42 +0800

    Add files via upload

---
+++ AssessmentTask3-2.py
@@ -0,0 +1,37 @@
+import random
+
+answer = random.randint(1, 100)
+guess = 0
+guesses = [0, 0]
+
+print("WELCOME TO THE GUESSING GAME!!.")
+print("I HAVE A NUMBER BETWEEN 1 AND 100.")
+print("IF YOUR FIRST GUESS IS WITHIN 10 OF THE NUMBER I PICKED,I WILL SAY WARM.")
+print("IF YOUR FIRST GUESS IS NOT WITHIN 10 OF THE NUMBER I PICKED, I WILL SAY COLD.")
+print("I WILL SAY YOU ARE  WARMER IF YOUR GUESS IS CLOSER THAN YOUR MOST RECENT GUESS.")
+print("I WILL SAY YOU ARE COLDER IF YOUR GUESS IS FURTHER THAN YOUR MOST RECENT GUESS.")
+
+while guess != answer:
+    print("GUESS BETWEEN 1 AND 100, WHAT'S YOUR GUESS?")
+    guess = int(input())
+    guesses.append(guess)
+    lastGuess = abs(guesses[-2] - answer)
+    currentGuess = abs(guesses[-1] - answer)
+
+    if guess == answer:
+        print("CONGRATULATIONS YOU WON! THE CORRECT ANSWER IS: ", answer)
+        print("IT TOOK YOU", len(guesses) - 2, "GUESSES TO GET THE CORRECT ANSWER")
+        break
+    if guess < 1 or guess > 100:
+        print("OUT OF BOUNDS")
+        print("PLEASE TRY AGAIN!")
+    if guesses[-2]:
+        if currentGuess < lastGuess:
+            print("Warmer")
+        elif currentGuess > lastGuess:
+            print("Colder")
+    else:
+        if abs(answer - guess) <= 10:
+            print('WARM!')
+        else:
+            print('COLD!')
