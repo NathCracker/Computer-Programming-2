1e3dd265e1b448fe5ef1255e810fba34ba9bfc80
Nath Dabne <101397760+NathCracker@users.noreply.github.com>
2023-01-13 11:42:29 +0800

    Update AssessmentTask8.py

--- AssessmentTask8.py
+++ AssessmentTask8.py
@@ -14,6 +14,15 @@ print(f'There are {len(str)} words in the sentence.')
 print(str)
 print(str['the'])
 
-
+#datetime
+import datetime
+t1 = datetime.time(4, 20, 1)
+t2 = datetime.time(6, 20, 1)
+print(t1)
+print("hour :", t1.hour)
+print("minute:", t1.minute)
+print("second :", t1.second)
+print("microsecond :", t1.microsecond)
+print("tzinfo :", t1.tzinfo)
 
 
